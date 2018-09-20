4b0341cc63033e4271ed5298cec16af3a8525724
jan-paul <janpaulmoolen99@gmail.com>
2018-09-20 09:35:02 +0100

    oefeningen voor FOR loops in python

---
+++ Les 5/opdrachten/Exercises_les_5.py
@@ -0,0 +1,15 @@
+age = eval(input('wat is uw leeftijd:'))
+
+if age > 62:
+    print ('you can get social security benefits')
+
+report = 'in this weeks loan report you are awaiting large bonuses'
+if 'large bonuses' in report:
+    print('vacation time')
+
+
+hits = eval(input('how much did you get hit for:'))
+shield = 0
+if hits > 10 and shield == 0:
+    print('you\'re dead')
+
